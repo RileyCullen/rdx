81d5ed62bac562074aaa2bb52b8353b4ae914f3f
Riley  Cullen <rileycullen@Rileys-MacBook-Pro.local>
2026-04-12 14:07:39 -0700

    chore: Remove unused files

---
+++ src/renderer/app.tsx
@@ -0,0 +1,17 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+
+function Hello() {
+  return (
+    <p>Hello, world</p>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
